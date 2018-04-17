c87671ea92dd015b7c4a304dace3c35159c62ada
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-04-17 13:19:35 +0800

    Complete 10085

--- problems/10085/job.c
+++ problems/10085/job.c
@@ -1,10 +1,12 @@
 #include "utils.h"
 
+#define OFFSET 32
+
 int ret[128];
 int run(int n, int key) {
     int sum = 0;
-    f(n, key, ret, ret+1, ret+2, ret+3);
+    f(n, key, ret, ret+OFFSET, ret+OFFSET*2, ret+OFFSET*3);
     for (int i = 0; i < 4; i++)
-        sum += ret[i];
+        sum += ret[OFFSET*i];
     return sum;
 }
