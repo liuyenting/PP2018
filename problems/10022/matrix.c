68365fa8fb5e9cdf37f2e4ea773b8598069e0b27
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-04-24 11:35:21 +0800

    Complete 10022

--- problems/10022/matrix.c
+++ problems/10022/matrix.c
@@ -1,12 +1,33 @@
 #include "matrix.h"
+#include <omp.h>
+
+#define MIN(x, y) (x < y ? x : y)
 
 void multiply(int N, unsigned long A[][2048], unsigned long B[][2048], unsigned long C[][2048]) {
+    // transpose
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            unsigned long sum = 0;    // overflow, let it go.
-            for (int k = 0; k < N; k++)
-                sum += A[i][k] * B[k][j];
-            C[i][j] = sum;
+        for (int j = i+1; j < N; j++) {
+            unsigned long tmp = B[j][i];
+            B[j][i] = B[i][j];
+            B[i][j] = tmp;
+        }
+    }
+
+    int N_THREADS = omp_get_num_threads();
+    int BLOCK = (N+N_THREADS-1)/N_THREADS;
+    #pragma omp parallel
+    {
+        int tid = omp_get_thread_num();
+        int lo = tid*BLOCK;
+        int hi = MIN(N, lo+BLOCK);
+        for (int i = lo, t = 0; i < hi; i++, t++) {
+            for (int j = 0; j < N; j++) {
+                unsigned long sum = 0;    // overflow, let it go.
+                for (int k = 0; k < N; k++) {
+                    sum += A[i][k] * B[j][k];
+                }
+                C[i][j] = sum;
+            }
         }
     }
 }
