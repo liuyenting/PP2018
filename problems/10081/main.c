0ee36d446c59be450d582b74b999173b0061aa58
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-05-15 14:58:16 +0800

    Complete 10081

--- problems/10081/main.c
+++ problems/10081/main.c
@@ -5,18 +5,17 @@
 
 #define SIDE 2048
 
-#define nLiveNeighbor(A, i, j) \
-    A[i + 1][j] + A[i - 1][j] + A[i][j + 1] + \
-    A[i][j - 1] + A[i + 1][j + 1] + A[i + 1][j - 1] + \
-    A[i - 1][j + 1] + A[i - 1][j - 1]
+#define nLiveNeighbor(G, f, i, j) \
+    G[f][i + 1][j] + G[f][i - 1][j] + G[f][i][j + 1] + \
+    G[f][i][j - 1] + G[f][i + 1][j + 1] + G[f][i + 1][j - 1] + \
+    G[f][i - 1][j + 1] + G[f][i - 1][j - 1]
 
-char A[SIDE][SIDE];
-char B[SIDE][SIDE];
+char G[2][SIDE][SIDE];
 
-void print(char A[SIDE][SIDE], int n) {
+void print(char G[2][SIDE][SIDE], int flag, int n) {
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++)
-            printf("%d", A[i][j]);
+            printf("%d", G[flag][i][j]);
         printf("\n");
     }
 }
@@ -25,44 +24,24 @@ int main() {
     int n, generation, cell;
     scanf("%d%d", &n, &generation);
     for (int i = 1; i <= n; i++) {
-        scanf("%s", &A[i][1]);
+        scanf("%s", &G[0][i][1]);
         for (int j = 1; j <= n; j++) {
-            A[i][j] = (A[i][j] == '0') ? 0 : 1;
+            G[0][i][j] -= '0';
         }
     }
 
-    int nln;
-    for (int g = 0; g < generation; g++) {
-        if (g % 2 == 0) {
-             /*  from A to B */
-#pragma omp parallel for collapse(2) \
-                         schedule(static, 128)
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    nln = nLiveNeighbor(A, i, j);
-                    B[i][j] = ((A[i][j] == 0 && nln == 3) ||
-                        (A[i][j] == 1 && (nln == 2 || nln == 3)));
-                }
-            }
-        } else {
-            /*  from B to A */
-#pragma omp parallel for collapse(2) \
-                         schedule(static, 128)
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    nln = nLiveNeighbor(B, i, j);
-                    A[i][j] = ((B[i][j] == 0 && nln == 3) ||
-                        (B[i][j] == 1 && (nln == 2 || nln == 3)));
-                }
+#pragma omp parallel
+    for (int g = 0, flag = 0; g < generation; g++, flag = 1-flag) {
+#pragma omp for
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int nln = nLiveNeighbor(G, flag, i, j);
+                G[1-flag][i][j] = ((G[flag][i][j] == 0 && nln == 3) ||
+                    (G[flag][i][j] == 1 && (nln == 2 || nln == 3)));
             }
         }
     }
-
-    if (generation % 2 == 0) {
-        print(A, n);
-    } else {
-        print(B, n);
-    }
+    print(G, generation%2, n);
 
     return 0;
 }
