0663bfd45704b15b0247fd69ca6279f375e3e9af
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-05-22 12:21:31 +0800

    Refactor

--- problems/10092/main.c
+++ problems/10092/main.c
@@ -4,29 +4,28 @@
 #include <CL/cl.h>
 
 int main(void) {
-    char filename[30];
-    scanf("%s", filename);
-    
     cl_int status;
 
-    // get platform id
+    // query platforms and devices
     cl_platform_id platform_id;
-    status = clGetPlatformIDs(1, &platform_id, NULL); 
+    status = clGetPlatformIDs(1, &platform_id, NULL);
     assert(status == CL_SUCCESS);
-    
-    // get device id
+
     cl_device_id device_id;
     status = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, NULL);
-    assert(status == CL_SUCCESS);;
-    
+    assert(status == CL_SUCCESS);
+
     // create context
     const cl_context_properties properties[] = {
-	CL_CONTEXT_PLATFORM, (cl_context_properties)platform_id,
-	0
+        CL_CONTEXT_PLATFORM, (cl_context_properties)platform_id,
+        0
     };
     cl_context context = clCreateContext(properties, 1, &device_id, NULL, NULL, &status);
     assert(status == CL_SUCCESS);
-    
+
+    char filename[30];
+    scanf("%s", filename);
+
     // create program
     unsigned char *program_file = NULL;
     size_t program_size = 0;
@@ -37,21 +36,27 @@ int main(void) {
     fseek(fp, 0, SEEK_SET);
     program_file = malloc(program_size);
     if (!program_file) {
-	fclose(fp); 
-	assert(program_file != 0);
+        fclose(fp);
+        assert(program_file != 0);
     }
     fread(program_file, program_size, 1, fp);
     fclose(fp);
-    
+
     cl_program program = clCreateProgramWithSource(context, 1, (const char **)&program_file, &program_size, &status);
     assert(status == CL_SUCCESS);
-    
+
+    // retrieve build log
     size_t len = 0;
     status = clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &len);
     char *build_log = calloc(len, sizeof(char));
     status = clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, len, build_log, NULL);
     printf("%s", build_log);
     free(build_log);
-    
+
+    // release resources
+    clReleaseProgram(program);
+    clReleaseContext(context);
+    clReleaseDevice(device);
+
     return 0;
 }
