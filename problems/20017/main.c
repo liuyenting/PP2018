cfd214e63665a865e763fb7a156c616baab3710d
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-05-29 10:09:08 +0800

    Fill out NDRangeKernel parms

--- problems/20017/main.c
+++ problems/20017/main.c
@@ -6,7 +6,8 @@
 
 #include <CL/cl.h>
 
-#define BLK_SIZE 256
+#define BLK_SIZE    256     // local buffer size
+#define BATCH_SIZE  256     // single submission
 
 static char *
 load_program_source(const char *filename) {
@@ -95,18 +96,35 @@ int main(int argc, char *argv[]) {
     d_buf = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(uint32_t)*8, NULL, &status);
     assert(status == CL_SUCCESS);
 
+    const int ZERO = 0;
     int N;
     uint32_t key1, key2;
     while (scanf("%d %" PRIu32 " %" PRIu32, &N, &key1, &key2) == 3) {
         status = CL_SUCCESS;
-        status |= clSetKernelArg(kernel, 0, sizeof(), &N);
-        status |= clSetKernelArg(kernel, 1, sizeof(), &key1);
-        status |= clSetKernelArg(kernel, 2, sizeof(), &key2);
+        status |= clSetKernelArg(kernel, 0, sizeof(int), &N);
+        status |= clSetKernelArg(kernel, 1, sizeof(uint32_t), &key1);
+        status |= clSetKernelArg(kernel, 2, sizeof(uint32_t), &key2);
         status |= clSetKernelArg(kernel, 3, sizeof(cl_mem), &d_buf);
         assert(status == CL_SUCCESS);
 
         // execute kernel
+        N = (N + BATCH_SIZE*BLK_SIZE - 1) / (BATCH_SIZE*BLK_SIZE) * BLK_SIZE;
 
+        status = clEnqueueFillBuffer(command, d_buf, &ZERO, sizeof(uint32_t), 0, sizeof(uint32_t)*8, NULL, NULL);
+        assert(status == CL_SUCCESS);
+
+        size_t global_size[] = { N };
+        size_t local_size[] = { BLK_SIZE };
+        status = clEnqueueNDRangeKernel(
+            command,
+            kernel,
+            1,              // work dimension
+            NULL,           // global offset
+            &global_size,   // global size
+            &local_size,    // local size
+            0, NULL, NULL   // events
+        );
+        assert(status == CL_SUCCESS);
 
         // read out
         uint32_t sum[8];
@@ -120,7 +138,7 @@ int main(int argc, char *argv[]) {
         printf("%" PRIu32 "\n", sum[0]);
     }
 
-    // release resources 
+    // release resources
     clReleaseMemObject(d_buf);
     clReleaseKernel(kernel);
     clReleaseProgram(program);
