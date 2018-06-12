1536f0f58a41ab264c00813c9425dd07f2c5f29a
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-06-12 12:01:56 +0800

    Accumulate first index

--- problems/20016/labeling.cu
+++ problems/20016/labeling.cu
@@ -1,6 +1,8 @@
 #include "labeling.h"
 
 #include <thrust/tabulate.h>
+#include <thrust/scan.h>
+#include <thrust/functional.h>
 #include <thrust/execution_policy.h>
 
 struct mark_spaces {
@@ -18,6 +20,10 @@ struct mark_spaces {
 };
 
 void labeling(const char *cuStr, int *cuPos, int strLen) {
+    /*
+        _ _ a  _ _ v  y  _ _ l  u  _  _  r  a  h
+        0 1 -1 3 4 -1 -1 7 8 -1 -1 11 12 -1 -1 -1
+     */
     thrust::tabulate(
         thrust::device,
         cuPos,          // beginning of the input sequence
@@ -25,4 +31,15 @@ void labeling(const char *cuStr, int *cuPos, int strLen) {
         mark_spaces(cuStr)
     );
 
+    /*
+        _ _ a _ _ v y _ _ l u _  _  r  a  h
+        0 1 1 3 4 4 4 7 8 8 8 11 12 12 12 12
+     */
+    thrust::inclusive_scan(
+        thrust::device,
+        cuPos,          // beginning of the input sequence
+        cuPos+strLen,   // end of the input sequence
+        cuPos,          // beginning of the output sequence, inplace
+        thrust::maximum<int>()
+    );
 }
